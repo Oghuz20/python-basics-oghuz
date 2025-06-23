804141488d47da5a7b225617338867c6e17ce520
Oghuz Hasanli <oguzhsnli2@gmail.com>
2025-06-23 17:23:42 +0400

    Add if-else and loop examples

---
+++ if_else_loops.py
@@ -0,0 +1,27 @@
+# -------------------------------
+# If-Else Statements and Loops
+# -------------------------------
+
+# If-Else
+score = 85
+if score >= 90:
+    print("Grade: A")
+elif score >= 80:
+    print("Grade: B")
+else:
+    print("Grade: C or lower")
+
+# While Loop
+counter = 0
+while counter < 5:
+    print("Counter is:", counter)
+    counter += 1
+
+# For Loop with range
+for i in range(3):
+    print("Iteration:", i)
+
+# Loop through a list
+fruits = ["apple", "banana", "cherry"]
+for fruit in fruits:
+    print("Fruit:", fruit)
